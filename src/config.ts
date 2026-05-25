31319440be858e5a7dcd365ef2e382f9a4dc0cb5
pykeguo <pykeguo@tencent.com>
2026-05-25 20:17:03 +0800

    chore(config): 更新站点URL与基础路径

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,12 @@ import type { AccentColor, BaseColor } from "./colors";
 // IMPORTANT SET THOSE TWO VARIABLES:
 // NUMBER ONE:
 // if deployed to github pages, set to https://<your-github-username>.github.io/
-export const SITE = "https://flo-bit.dev";
+export const SITE = "https://chuhsing.com";
 
 // NUMBER TWO:
 // if repo name is <your-github-username>.github.io set to '/'
 // otherwise set to '/<your-repo-name>'
-export const BASE = "/blog-template";
+export const BASE = "";
 
 // MORE SETTINGS:
 
@@ -16,7 +16,8 @@ export const BASE = "/blog-template";
 export const SITE_TITLE = "flo-bit's blog template";
 
 // will be used in the meta tags (and for example shown in search results)
-export const SITE_DESCRIPTION = "Welcome to my blog template! Fork it and make it your own.";
+export const SITE_DESCRIPTION =
+  "Welcome to my blog template! Fork it and make it your own.";
 
 // will be used as the icon in the header and the favicon
 export const SITE_FAVICON = "🙃";
@@ -30,7 +31,7 @@ export const LICENSE = "MIT licensed.";
 // will be used to identify your bluesky account, so that likes and comments can be shown on your posts
 export const BLUESKY_IDENTIFIER = "flo-bit.dev";
 
-export const SOURCE_LINK = 'https://github.com/flo-bit/blog-template';
+export const SOURCE_LINK = "https://github.com/flo-bit/blog-template";
 
 // will be used to set the base color of the blog
 export const BASE_COLOR: BaseColor = "neutral";
@@ -56,7 +57,6 @@ export const SOCIAL_LINKS: {
   GITHUB_URL: "https://github.com/flo-bit",
   EMAIL: "flo.bit.dev@gmail.com",
 };
-
 
 // EVEN MORE SETTINGS:
 
